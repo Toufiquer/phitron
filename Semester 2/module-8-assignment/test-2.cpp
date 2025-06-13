6c5e6f966bfe530f43efe319e1e7af274e79f1fc
Toufiquer <toufiquerabc@gmail.com>
2025-06-13 09:18:55 +0600

    text-2.1.1

---
+++ Semester 2/module-8-assignment/test-2.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string S;
+    getline(cin,S);
+    stringstream ss(S);
+    string w;
+    int a = 0; // ! 0 ->> false, 1 ->> true
+    while(ss >> w){
+        if(a==1){
+            break;
+        } 
+      string fixedName = "Jessica";
+      int lenChecking = w.length();
+      for (int i = 0; i < lenChecking;i++){
+        if(fixedName[i] == w[i]){
+            a = 1;
+        }else{
+            a =0;
+            break;
+        }
+      } 
+    }
+
+    if(a==1){
+        cout << "YES";
+    }else{
+        cout << "NO";
+    }
+
+    return 0;
+}
