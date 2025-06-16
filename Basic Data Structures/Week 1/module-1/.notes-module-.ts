d26d6f21c0010adbc15719948b870b30ae3e4ea2
Toufiquer <toufiquerabc@gmail.com>
2025-06-16 15:48:30 +0600

    start Basic Data Structures

---
+++ Basic Data Structures/Week 1/module-1/.notes-module-.ts
@@ -0,0 +1,41 @@
+// ! video - 0
+// 1. introduction
+
+// ! video - 1
+// 1. Why we need to learn time complexity
+
+// ! video - 2
+// 1. What is time complexity
+
+// ! video - 3
+// 1. How to calculate time complexity
+
+// ! video - 4
+// 1. Linear Complexity O(N)
+
+// ! video - 5
+// 1. Logarithmic Complexity O(logN)
+
+// ! video - 6
+// 1. Sqrt complexity O(sqrt(N))
+
+// ! video - 7
+// 1. Quadratic complexity O(N_N)
+
+// ! video - 8
+// 1. Linearithmic complexity O(NlogN)
+
+// ! video - 9
+// 1. Best to Worst Complexity
+
+// ! video - 10
+// 1. Solving time complexity related problems
+
+// ! video - 11
+// 1. How to calculate time from time complexity
+
+// ! video - 12
+// 1. Space Complexity
+
+// ! video - 13
+// 1. Summary 
