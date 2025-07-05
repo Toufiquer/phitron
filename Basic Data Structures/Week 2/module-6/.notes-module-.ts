ec84f80a80a2213268697c028f0a15c11f1050e5
tousec <toufiquer.sec@gmail.com>
2025-07-05 10:28:53 +0600

    1

---
+++ Basic Data Structures/Week 2/module-6/.notes-module-.ts
@@ -0,0 +1,69 @@
+// ! video - 0
+// 1. introduction
+
+// ! video - 1
+// 1.
+
+// ! video - 2
+// 1.
+
+// ! video - 3
+// 1.
+
+// ! video - 4
+// 1.
+
+// ! video - 5
+// 1.
+
+// ! video - 6
+// 1.
+
+// ! video - 7
+// 1.
+
+// ! video - 8
+// 1.
+
+// ! video - 9
+// 1.
+
+// ! video - 10
+// 1.
+
+// ! video - 11
+// 1.
+
+// ! video - 12
+// 1.
+
+// ! video - 13
+// 1.
+
+// ! video - 14
+// 1.
+
+// ! video - 15
+// 1.
+
+
+// !! Linked List Operations
+// 1. Insertion:
+//  - at head
+//  - at tail
+//  - at any position
+
+// 1. Deletion:
+//  - head
+//  - tail
+//  - any position
+
+// 2. Printing:
+//  - forward
+//  - backward
+
+// 3. Sorting
+//  - Ascending
+//  - Descending
+
+// 4. 
---
+++ Basic Data Structures/Week 2/module-6/test.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fun(){
+    *p = 100;
+    // cout << "In fun: " << *p << endl;
+}
+
+int main(){
+    // !
+    int x = 10;
+    int *p = &x;
+    fun(p);
+    cout << "In main: " << *p << endl;
+    cout << x << endl;
+
+    return 0;
+}
