8040f222dd8a5f2f940381e3577a0165c546b543
Toufiquer <toufiquerabc@gmail.com>
2025-07-10 02:10:12 +0600

    done @ m9

---
+++ Basic Data Structures/Week 3/module-10/.notes-module-.ts
@@ -0,0 +1,47 @@
+// ! video - 0
+// 1. introduction
+
+// ! video - 1
+// 1. List Constructors
+
+// ! video - 2
+// 1. List Capacity functions
+
+// ! video - 3
+// 1. List Modifiers functions
+
+// ! video - 4
+// 1. List Modifiers functions II
+
+// ! video - 5
+// 1. List operation functions
+
+// ! video - 6
+// 1. List element access and iterators
+
+// ! video - 7
+// 1. Idea of reversing singly linked list
+
+// ! video - 8
+// 1. Reverse singly linked list Implementation
+
+// ! video - 9
+// 1. Reverse singly linked list Animated
+
+// ! video - 10
+// 1. Reverse Doubly linked list
+
+// ! video - 11
+// 1. Reverse doubly linked list Animated
+
+// ! video - 12
+// 1. Idea of detecting cycle in linked list
+
+// ! video - 13
+// 1. Detect cycle implementation
+
+// ! video - 14
+// 1. Detect cycle Animated.mp4
+
+// ! video - 15
+// 1. Summary
